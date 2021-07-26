d279004928a18ba547c4a823aa6d9bb583467d4b
DeepalKumarasiri <75887859+DeepalKumarasiri@users.noreply.github.com>
2021-07-26 16:39:30 +0530

    Update read.js

--- read.js
+++ read.js
@@ -5,7 +5,7 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/sample_airbnb?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://Gamy_Gamin:Gamy_Gamin@cluster0.wujnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
     /**
      * The Mongo Client you will use to interact with your database
