45cb10648024689798ae49cf78345eb72905a772
DeepalKumarasiri <75887859+DeepalKumarasiri@users.noreply.github.com>
2021-07-26 17:12:00 +0530

    Update read.js

--- read.js
+++ read.js
@@ -23,7 +23,7 @@ async function main() {
         // Make the appropriate DB calls
 
         // Find the listing named "Infinite Views" that we created in create.js
-        await findOneListingByName(client, "Infinite Views");
+        await findOneListingByName(client, "avengers");
 
         // Find up to 5 listings with at least 4 bedrooms and at least 2 bathrooms
         // If you recently ran create.js, a listing named Beautiful Beach House should be included in the results 
