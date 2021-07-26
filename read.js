e1699d9e4bf86e93d0cc71849397f53826f15847
DeepalKumarasiri <75887859+DeepalKumarasiri@users.noreply.github.com>
2021-07-26 19:31:53 +0530

    Update read.js

--- read.js
+++ read.js
@@ -49,7 +49,7 @@ main().catch(console.error);
  */
 async function findOneListingByName(client, nameOfListing) {
     // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-    const result = await client.db("Cluster0").collection("-1001272450387").findOne({ "file_name": nameOfListing });
+    const result = await client.db("Cluster0").collection("-1001272450387").find({ "file_name": nameOfListing });
 
     if (result) {
         console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
